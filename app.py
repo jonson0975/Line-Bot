ea3504c31c460242c96810d6c02165fddebdb143
jonson0975 <98145589+jonson0975@users.noreply.github.com>
2022-03-06 09:49:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+import re
 
 app = Flask(__name__)
  
