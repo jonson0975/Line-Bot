31bdd2b52586cfab1014c07ea1e276c183c71dfe
jonson0975 <98145589+jonson0975@users.noreply.github.com>
2022-03-06 10:38:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 def prepare_record(message):
-    text_list = message.split('@')   
+    text_list = message.split('\n')   
 
     record_list = []
 
