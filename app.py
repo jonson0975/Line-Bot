7e410180564a7acefc78e6f2324d3c43045a719f
jonson0975 <98145589+jonson0975@users.noreply.github.com>
2022-03-06 09:48:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,11 @@ def callback():
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    if "記錄" in message:
-        line_bot_api.reply_message(event.reply_token, message)
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴你哩！'))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
