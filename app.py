9bbeef2394d86c8d4f5862d1836250d9229c106d
jonson0975 <98145589+jonson0975@users.noreply.github.com>
2022-03-06 09:09:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # 載入LineBot所需要的模組
-import re
-import os
-import psycopg2
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import *
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from flask import Flask, request, abort, render_template
-from linebot.models.responses import Content
 
 app = Flask(__name__)
 
@@ -35,98 +36,15 @@ def callback():
 
     return 'OK'
 
-# handle text message
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    if "記錄" in msg:
-        try:
-            record_list = prepare_record(msg)
-            result = insert_record(record_list)
-
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=result)
-            )
-        except:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="資料上傳失敗")
-            )
-#     elif "查詢" in msg:
-#         result = select_record()
-
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=result)
-#         )
-#     elif "刪除" in msg:
-#         result = delete_record(msg)
-
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=result)
-#         ) 
-#     elif "更新" in msg:
-#         result = update_record(msg)
-
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=result)
-#         ) 
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=msg)
-        )
-
-def prepare_record(msg):
-    text_list = msg.split('@')
-    record_list = []
-
-    for i in text_list[1:]:
-        temp_list = i.split(" ")
-
-        userid = temp_list[0]
-        writingdate = temp_list[1]
-        diary = temp_list[2]
-
-        record = (userid, writingdate, diary)
-        record_list.append(record)
-
-    return record_list
-
-# 將資料匯入資料庫
-def insert_record(record_list):
-    DATABASE_URL = os.environ["DATABASE_URL"]
-
-    conn = psycopg2.connect(database="dahggat84j3plu",
-						    user="fmhvtfdwhmriha",
-						    password="6fa7397e002c2217f7975b7fe04e8348d7f14966c49137f500b6e9ba3f22b796",
-						    host="ec2-35-175-68-90.compute-1.amazonaws.com",
-						    port="5432")
-    cursor = conn.cursor()
-
-    table_columns = "(userid, writingdate, diary)"
-    postgres_insert_query = f"""INSERT INTO userdiary {table_columns} VALUES (%s,%s,%s)"""
-
-    try:
-        cursor.executemany(postgres_insert_query, record_list)
-    except:
-        cursor.execute(postgres_insert_query, record_list)
-
-    conn.commit()
-
-    # 要回傳的文字
-    message = f"{cursor.rowcount}筆資料成功匯入資料庫囉"
-
-    cursor.close()
-    conn.close()
-
-    return message
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+    	app.run(host='0.0.0.0', port=port)
