5e9a3f28906b0ae8e0476b82d21790e342066a52
jonson0975 <98145589+jonson0975@users.noreply.github.com>
2022-03-08 16:08:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,21 +59,21 @@ def handle_message(event):
 #             preview_image_url='https://imgur.com/a/6yuwnTL'
 #         )
 #         line_bot_api.reply_message(event.reply_token, image_message)
-#     elif "使用者" in message:
-#         link = 'http://10.1.4.189:5000/app_test/'
-#         message = user_id(message)
-#         result = link + message
-#         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
+    elif "使用者" in message:
+        link = 'http://10.1.4.189:5000/app_test/'
+        message = user_id(message)
+        result = link + message
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
-# def user_id(message):
-#     characters = "使用者"
+def user_id(message):
+    characters = "使用者"
 
-#     for x in range(len(characters)):
-#         message = message.replace(characters[x],"")
+    for x in range(len(characters)):
+        message = message.replace(characters[x],"")
 
-#     return message      
+    return message      
       
 def prepare_record(message):
     text_list = message.split('\n')   
